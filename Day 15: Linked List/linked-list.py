9dd39a9ea166e8533b04d7eb23274396cfa87036
PritaM <47033786+Pritam055@users.noreply.github.com>
2021-12-01 20:40:36 +0545

    added insert function for linked list

--- Day 15: Linked List/linked-list.py
+++ Day 15: Linked List/linked-list.py
@@ -1,13 +1,25 @@
 
 
-    def insert(self,head,data): 
-    #Complete this method
-        if head is None:
-            head = Node(data)
-            self.tail = head
-        else:
-            node = Node(data)
-            self.tail.next = node
-            self.tail = node
-        return head
-        
+""" def insert(self,head,data): 
+#Complete this method
+    if head is None:
+        head = Node(data)
+        self.tail = head
+    else:
+        node = Node(data)
+        self.tail.next = node
+        self.tail = node
+    return head
+"""
+
+def insert(self, head, data):
+    if head is None:
+        head = Node(data)
+        return head 
+   
+    curr = head 
+    while curr.next:
+        curr = curr.next 
+    curr.next = Node(data)
+    return head 
+    
